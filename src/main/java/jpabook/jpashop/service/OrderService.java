00bb5a5131934974ddeee063a6260e28fd142ceb
hyowon6 <hyowonng@gmail.com>
2023-05-17 01:26:37 +0900

    [clear] 6-3

---
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -0,0 +1,64 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Delivery;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import jpabook.jpashop.repository.MemberRepository;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderService {
+    private final OrderRepository orderRepository;
+    private final MemberRepository memberRepository; //memberId
+    private final ItemRepository itemRepository; //itemId
+
+    //주문
+    @Transactional//데이터를 변경하는 것
+    public Long order(Long memberId, Long itemId, int count) {//멤버, 아이템, 개수
+        //엔티티 조회
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemRepository.findOne(itemId);
+
+        //배송정보 생성
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());//회원의 주소 값으로 배송을 한다
+
+        //주문상품 생성
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);//생성메서드 사용
+        //주문할 때 주문 상품은 하나만 넘기도록 함
+
+        //주문 생성
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        //주문 저장
+        orderRepository.save(order);
+        //cascade 옵션이 있기 때문에 orderitem과 delivery 자동으로 함께 persist되면서
+        //트랜잭션이 커밋되는 시점에 DB 테이블에 저장
+        return order.getId();
+
+    }
+
+    //주문 취소
+    @Transactional
+    public void cancelOrder(Long orderId) {
+        //주문 엔티티 조회
+        Order order = orderRepository.findOne(orderId);//orderId를 찾음
+        //주문 취소
+        order.cancel();//order에서 만든 비지니스 로직 사용
+    }
+
+    //검색
+//    public List<Order> findOrders(OrderSearch orderSearch) {
+//        return orderRepository.findAll(orderSearch);
+//    }
+}
