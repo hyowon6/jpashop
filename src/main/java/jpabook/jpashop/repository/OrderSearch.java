b6c79159256a6ccfbca62dbc6213319712fcb73a
hyowon6 <hyowonng@gmail.com>
2023-05-19 01:19:56 +0900

    [clear] 7-1

---
+++ src/main/java/jpabook/jpashop/repository/OrderSearch.java
@@ -0,0 +1,11 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class OrderSearch {//이 파라미터 조건이 있으면 where문으로 검색이 되야함
+    private String memberName; //회원 이름
+    private OrderStatus orderStatus;//주문 상태[ORDER, CANCEL]
+}
